ad2d3caa9d8894448e2f0c1bd17b4ba4766d28de
john <nergilz@yandex.ru>
2018-04-27 00:22:29 +0200

    first version

--- README.md
+++ README.md
@@ -1,7 +1,50 @@
-# Github Trends
+## Github Trends
 
-[TODO. There will be project description]
+---
+This script shows trends repositories and their issues on github.com  
+---
 
-# Project Goals
+
+### Discription
+
++ The script show twenty trends repositories in the last week
++ Show the count issues for repositories
++ Show the URL and Username for repositories
++ You make exchange count trends repo and days 
+
+
+### Start script example
+
+```bash
+python3 github_trending.py
+```
+```bash
+python3 github_trending.py 10 5 
+```
+
+### Example result
+
+```bash
+User: will URL: https://api.github.com/repos/will/slacktyping Repositories: will/slacktyping Issues: 0
+User: francoispqt URL: https://api.github.com/repos/francoispqt/gojay Repositories: francoispqt/gojay Issues: 4
+User: hasura URL: https://api.github.com/repos/hasura/awesome-live-reloading Repositories: hasura/awesome-live-reloading Issues: 3
+User: codrops URL: https://api.github.com/repos/codrops/ParticleEffectsButtons Repositories: codrops/ParticleEffectsButtons Issues: 0
+User: yaochao URL: https://api.github.com/repos/yaochao/img_to_txt Repositories: yaochao/img_to_txt Issues: 1
+ Script Time: 33.90102304500033
+```
+
+### Get help
+```bash
+python3 github_trending.py --help
+```
+
+### Requirements
+```bash
+Python ver 3.5 (or higher)
+```
+
+---
+
+### Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,85 @@
+import requests
+import datetime
+import timeit
+import argparse
 
 
-def get_trending_repositories(top_size):
-    pass
+def get_trending_repositories(top_stars, days_ago):
+
+    date_ago = datetime.date.today() - datetime.timedelta(days_ago)
+    request_params = {
+        'q': '{}{}'.format('created:>', date_ago.strftime('%Y-%m-%d')),
+        'sort': 'stars',
+        'order': 'desc',
+        'per_page': top_stars
+    }
+    request = requests.get(
+        'https://api.github.com/search/repositories',
+        params=request_params
+    )
+    if request.status_code == requests.codes.ok:
+        return request.json()['items']
+    else:
+        return None
 
 
-def get_open_issues_amount(repo_owner, repo_name):
-    pass
+def get_open_issues_amount(repo_name):
+
+        request = requests.get(
+            'https://api.github.com/repos/{}/issues'.format(repo_name)
+            )
+        return len(request.json())
+
+
+def pprint_data(request_json):
+
+    for repo in request_json:
+        repo_name = repo.get('full_name')
+        repo_owner = repo.get('owner').get('login')
+        repo_url = repo.get('url')
+        issues_count = get_open_issues_amount(repo_name)
+        print('User: {0} URL: {1} Repositories: {2} Issues: {3}\n'.format(
+            repo_owner,
+            repo_url,
+            repo_name,
+            issues_count
+            )
+        )
+
+
+def get_parser_args():
+
+    parser = argparse.ArgumentParser(
+        description='This url to repositories, top_count and days_ago arguments for scripts'
+    )
+    parser.add_argument(
+        'top_stars',
+        type=int,
+        nargs='?',
+        default=20,
+        help='Count of top repositories'
+    )
+    parser.add_argument(
+        'days_ago',
+        type=int,
+        nargs='?',
+        default=7,
+        help='How many days ago created repositories'
+    )
+    return parser.parse_args()
+
 
 if __name__ == '__main__':
-    pass
+
+    arguments = get_parser_args()
+    start_time = timeit.default_timer()
+
+    request_json = get_trending_repositories(
+        arguments.top_stars,
+        arguments.days_ago
+        )
+    if request_json is not None:
+        pprint_data(request_json)
+    else:
+        print(' Not data in request!')
+    print(' Script Time: {}'.format(timeit.default_timer() - start_time))
