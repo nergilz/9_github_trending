6afe38e2398959720ee47ec281743c97a0f38c41
john <nergilz@yandex.ru>
2018-04-29 01:39:33 +0200

    all functions return data

--- github_trending.py
+++ github_trending.py
@@ -5,7 +5,7 @@ import argparse
 from collections import namedtuple
 
 
-def get_response_trends_repos(top_stars, days_ago):
+def get_trend_repos(top_stars, days_ago):
 
     date_ago = datetime.date.today() - datetime.timedelta(days_ago)
     request_params = {
@@ -14,16 +14,18 @@ def get_response_trends_repos(top_stars, days_ago):
         'order': 'desc',
         'per_page': top_stars
     }
-    return requests.get(
+    response = requests.get(
         'https://api.github.com/search/repositories',
         params=request_params
         )
+    return response.json()['items']
 
 
-def get_response_issues_repos(repo_name):
+def get_issues_repos(repo_name):
 
-    return requests.get(
+    response = requests.get(
         'https://api.github.com/repos/{}/issues'.format(repo_name))
+    return len(response.json())
 
 
 def get_data_on_trend_repos(trend_repos):
@@ -34,8 +36,7 @@ def get_data_on_trend_repos(trend_repos):
         repo_owner = repo['owner']['login']
         repo_name = repo['full_name']
         repo_url = repo['url']
-        response_issues = get_response_issues_repos(repo_name)
-        issues_count = len(response_issues.json())
+        issues_count = get_issues_repos(repo_name)
         urls_issues_names_repos.append(
             Data_repo(
                 repo_owner,
@@ -46,10 +47,10 @@ def get_data_on_trend_repos(trend_repos):
     return urls_issues_names_repos
 
 
-def pprint_repos_data(url_issues_names_repos):
+def pprint_data_repos(urls_issues_names_repos):
 
     print()
-    for index, data_repo in enumerate(url_issues_names_repos, start=1):
+    for index, data_repo in enumerate(urls_issues_names_repos, start=1):
         print(
             index,
             'User: {} URL: {} Repo: {} Issues: {}\n'.format(
@@ -88,13 +89,12 @@ if __name__ == '__main__':
     start_time = timeit.default_timer()
 
     try:
-        response_trends = get_response_trends_repos(
+        trend_repos = get_trend_repos(
             arguments.top_stars,
             arguments.days_ago
             )
-        trend_repos = response_trends.json()['items']
         urls_issues_names_repos = get_data_on_trend_repos(trend_repos)
-        pprint_repos_data(urls_issues_names_repos)
+        pprint_data_repos(urls_issues_names_repos)
 
     except requests.exceptions.HTTPError as error:
         print(' Requests ERROR:', error)
